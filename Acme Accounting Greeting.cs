1a929acc5390d7fafa4568c5e54a261e91960566
Ben <ben.s.becker@gmail.com>
2018-06-22 20:49:03 -0700

    send
    send

---
+++ Acme Accounting Greeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Acme_Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.ReadLine();
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+     
+       
+    }
+}
